70f2ad3b9f5ef10805ecdf00cc2768d3be89f2b6
DenysKhm <106743435+DenysKhm@users.noreply.github.com>
2022-08-18 09:53:51 +0000

    Deploying to gh-pages from @ DenysKhm/goit-js-project_team3@8edbda1ee4ae141c863935cd608ef9b2069bb2a3 🚀

--- index.f2e0aeb4.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("visually-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.f2e0aeb4.js.map
